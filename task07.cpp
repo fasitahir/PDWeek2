6c3959319d7c07c30c6a76bd5fed393c0acc2cb2
AbdulMannan <fasitahir2019@gmail.com>
2022-12-16 18:54:22 +0500

    Programming day week 2 Assignments

---
+++ task07.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+main(){system("color 06");
+cout<< "     ..::---::..      " <<endl;
+cout<< "   .--------------.                " <<endl;
+cout<< "  .----------------.                   " <<endl;
+cout<< " .---------------:.                   " <<endl;
+cout<< " .------------:.                    " <<endl;
+cout<< "  .----------:.                   " <<endl;
+cout<< "   .----------:.                  " <<endl;
+cout<< "    .------------:.                 " <<endl;
+cout<< "      .:----------:.                            " <<endl;
+}
---
+++ task11.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+main(){
+cout<<"                  ::::::::::::::::                  "<<endl;                      
+cout<<"                 ::      ||      ::                 "<<endl;
+cout<<"                ::       ||       ::                "<<endl;
+cout<<"               ::        ||        ::               "<<endl;         
+cout<<"   :::::::::::::_________||_________::::::::::::    "<<endl;
+cout<<"   ::                                         :)    "<<endl;
+cout<<"   ::                                         ::    "<<endl;
+cout<<"   ::::::  :::::::::::::::::::::::::::  ::::::::    "<<endl;
+cout<<"        (O)                           (O)           "<<endl;
+cout<<"                                                    "<<endl;
+cout<<"                                                    "<<endl;}
